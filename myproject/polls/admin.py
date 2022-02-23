441e315e2e24f630236bbd4be257b02ce751c9a8
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-02-23 01:01:31 -0300

    project init

--- myproject/polls/admin.py
+++ myproject/polls/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+
+admin.site.register(Question)
+
+from .models import Choice
+
+admin.site.register(Choice)
